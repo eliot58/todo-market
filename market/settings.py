0937db9def7c001e39a3116c92df4c6e0d2156c6
Aiomedin Chorobaev <eliothab58@gmail.com>
2023-12-21 14:05:29 +0600

    add req

--- market/settings.py
+++ market/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-p985ynt*jk^+qq*e=yz^&9)^s3%g7i7glar42@cmcba-gxy0)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,7 +117,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'core/static/'),
+    ]
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+    MEDIA_URL = '/media/'  
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+else:
+    MEDIA_ROOT = '/var/www/market/media'
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = '/var/www/market/static'
+    STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
