47ada7e7c4f67d42e0de5c654cb7b4dd9ef2b30b
Aiomedin Chorobaev <eliothab58@gmail.com>
2024-05-29 22:47:50 +0300

    delete tickers

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,6 +1,11 @@
 from .models import Ticker, News, Category
+from django.utils import timezone
 
 def ticker(_):
+    today = timezone.now()
+
+    Ticker.objects.filter(visible_date__lt=today).delete()
+    
     try:
         return {"tickers": Ticker.objects.all()}
     except Ticker.DoesNotExist:
