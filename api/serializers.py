daa23e145244195d14eb88efe2ff1fd34ab8f496
Aiomedin Chorobaev <eliothab58@gmail.com>
2024-06-15 12:13:00 +0600

    fix

--- api/serializers.py
+++ api/serializers.py
@@ -79,6 +79,7 @@ class NewsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
     class Meta:
         model = Product
         fields = '__all__'
