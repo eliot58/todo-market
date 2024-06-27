99d1c9e6b23dfc37ce137d21de34d7998cd41755
Aiomedin Chorobaev <eliothab58@gmail.com>
2024-06-27 13:04:03 +0600

    fix

--- market/celery.py
+++ market/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tododo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
 
 
 app = Celery('market')
